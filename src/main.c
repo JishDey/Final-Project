e576c239913022564f29e897086d8783057b1964
Jishnu Dey <jishdey05@gmail.com>
2025-04-28 16:34:24 -0400

    comment about clock enable

--- src/main.c
+++ src/main.c
@@ -10,8 +10,8 @@ int main() {
     host_serial_init();
     gpio_config_pullup(A3, PULL_OFF);
     gpio_config_mode(A3, ANALOG);
-    DAC1->CR = 0x0; //reset, just in case.
-    RCC->APB1ENR1 |= RCC_APB1ENR1_DAC1EN; //dac clock enable
+    DAC1->CR = 0x0; //reset DAC control register, just in case.
+    RCC->APB1ENR1 |= RCC_APB1ENR1_DAC1EN; //dac clock enable - not sure if we need this
     DAC1->CR |= (0b000 << DAC_CR_TSEL1_Pos); //set trigger source tim6. see 17.4.6 for bit codes
     DAC1->CR |= DAC_CR_TEN1; //enable the trigger source
     DAC1->CR |= (0b10 << DAC_CR_WAVE1_Pos); //enables the triangle wave
