9719b3e98490d78227c630fe099a5267f2ddf28b
regelFish <168052544+regelFish@users.noreply.github.com>
2025-04-29 17:05:49 -0400

    It's working ig

--- src/main.c
+++ src/main.c
@@ -1,28 +1,50 @@
 #include <stm32l432xx.h>
 #include "ee14lib.h"
+#include <math.h>
 
 int _write(int file, char *data, int len){
     serial_write(USART2, data, len);
     return len;
 }
 
+void DAC_init(void) {
+    DAC1->CR = 0x0; //reset, just in case.
+    DAC1->CR |= 0x1; // Enabling DAC1
+    DAC1->CR &= ~DAC_CR_TEN1; //disable the trigger source
+    DAC1->CR |= (0b00 << DAC_CR_WAVE1_Pos); //disables wave gen
+    DAC1->CR |= (0b0111 << DAC_CR_MAMP1_Pos); //triangle wave max amplitude ( >= 1011)
+}
+
 int main() {
     host_serial_init();
+    SysTick_initialize();
     gpio_config_pullup(A3, PULL_OFF);
     gpio_config_mode(A3, ANALOG);
     RCC->APB1ENR1 |= RCC_APB1ENR1_DAC1EN; //dac clock enable
     RCC->APB1ENR1 |= RCC_APB1ENR1_TIM6EN;
-    TIM6->CR1 = 0;
-    TIM6->PSC = 4;
-    TIM6->ARR = 1;
-    TIM6->CR2 &= ~TIM_CR2_MMS;
-    TIM6->CR2 |= (0x2 << TIM_CR2_MMS_Pos); // Set TRGO on update event
-    TIM6->CR1 |= TIM_CR1_CEN; 
-    DAC1->CR = 0x0; //reset, just in case.
-    DAC1->CR |= 0x1; // Enabling DAC1
-    DAC1->CR |= DAC_CR_TEN1; //enable the trigger source
-    DAC1->CR |= (0b10 << DAC_CR_WAVE1_Pos); //enables the triangle wave
-    DAC1->CR |= (0b0111 << DAC_CR_MAMP1_Pos); //triangle wave max amplitude ( >= 1011)
+    signed int sin_table[180];
+    float sf;
+    DAC_init();
+    for (int i = 0; i < 180; i++){
+        sf = sin(3.1415926 * (i + 90)/180);
+        sin_table[i] = (1+sf) * 2048;
+        if(sin_table[i] == 0x1000){
+            sin_table[i] = 0xFFF;
+        }
+    }
+    int i = 0;
+    while (true) {
+        i++;
+        DAC1->DHR12R1 = sin_table[i];
+        for (volatile int j = 0; j < 5; j++){}
+        if (i == 180) {
+            while (i > 0) {
+                DAC1->DHR12R1 = sin_table[i];
+                i--;
+                for (volatile int j = 0; j < 5; j++){}
+            }
+        }
+    }
     // DAC1->CR &= ~(DAC_CR_WAVE1 | DAC_CR_MAMP1 | DAC_CR_TSEL1); // Clear WAVE, MAMP, TSEL
     // DAC1->CR |= DAC_CR_WAVE1_1;          // Enable triangle wave generation (WAVE1 = 0b10)
     // DAC1->CR |= (0b1000 << DAC_CR_MAMP1_Pos); // Max amplitude (MAMP1 = 0x7 → 4095 steps)
